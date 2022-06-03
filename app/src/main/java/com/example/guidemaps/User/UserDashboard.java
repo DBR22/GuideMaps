4ccf38fef455101d9f261501e886b82f8c1bbb6a
davidblarig <75780229+davidblarig@users.noreply.github.com>
2022-06-04 00:51:13 +0200

    Navigation Drawer

--- app/src/main/java/com/example/guidemaps/User/UserDashboard.java
+++ app/src/main/java/com/example/guidemaps/User/UserDashboard.java
@@ -1,6 +1,7 @@
 package com.example.guidemaps.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,16 +9,27 @@ import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.example.guidemaps.HelperClasses.HomeAdapter.CategoriesAdapter;
+import com.example.guidemaps.HelperClasses.HomeAdapter.CategoriesHelperClass;
 import com.example.guidemaps.HelperClasses.HomeAdapter.FeaturedAdapter;
 import com.example.guidemaps.HelperClasses.HomeAdapter.FeaturedHelperClass;
+import com.example.guidemaps.HelperClasses.HomeAdapter.MostViewedAdapter;
+import com.example.guidemaps.HelperClasses.HomeAdapter.MostViewedHelperClass;
 import com.example.guidemaps.R;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
 public class UserDashboard extends AppCompatActivity {
 
-    RecyclerView featuredRecycler;
+    //Variables
+    RecyclerView featuredRecycler, mostViewedRecycler, categoriesRecycler;
     RecyclerView.Adapter adapter;
+    private GradientDrawable gradient1, gradient2, gradient3, gradient4;
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +38,57 @@ public class UserDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_user_dashboard);
 
         featuredRecycler = findViewById(R.id.featured_recycler);
+        mostViewedRecycler = findViewById(R.id.most_viewed_recycler);
+        categoriesRecycler = findViewById(R.id.categories_recycler);
 
+        //Menu
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        //Recycler Views
         featuredRecycler();
+        mostViewedRecycler();
+        categoriesRecycler();
+    }
+
+    private void categoriesRecycler() {
+
+        //All Gradients
+        gradient2 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffd4cbe5, 0xffd4cbe5});
+        gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xff7adccf, 0xff7adccf});
+        gradient3 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xfff7c59f, 0xFFf7c59f});
+        gradient4 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffb8d7f5, 0xffb8d7f5});
+
+
+        ArrayList<CategoriesHelperClass> categoriesHelperClasses = new ArrayList<>();
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1, R.drawable.school_image, "Education"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient2, R.drawable.hospital_image, "HOSPITAL"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient3, R.drawable.restaurant_image, "Restaurant"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient4, R.drawable.shopping_image, "Shopping"));
+        categoriesHelperClasses.add(new CategoriesHelperClass(gradient1, R.drawable.transport_image, "Transport"));
+
+
+        categoriesRecycler.setHasFixedSize(true);
+        adapter = new CategoriesAdapter(categoriesHelperClasses);
+        categoriesRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        categoriesRecycler.setAdapter(adapter);
+
+    }
+
+    private void mostViewedRecycler() {
+
+        mostViewedRecycler.setHasFixedSize(true);
+        mostViewedRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        ArrayList<MostViewedHelperClass> mostViewedLocations = new ArrayList<>();
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.mcdonald_img, "McDonald's"));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.city_2, "Edenrobe"));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.city_1, "J."));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.mcdonald_img, "Walmart"));
+
+        adapter = new MostViewedAdapter(mostViewedLocations);
+        mostViewedRecycler.setAdapter(adapter);
+
     }
 
     private void featuredRecycler() {
