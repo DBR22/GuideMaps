ed2598b2f9fea62f84bf2a50fdcfd74a9ddb9125
davidblarig <75780229+davidblarig@users.noreply.github.com>
2022-06-03 21:23:26 +0200

    Recyclerview

--- app/src/main/java/com/example/guidemaps/User/UserDashboard.java
+++ app/src/main/java/com/example/guidemaps/User/UserDashboard.java
@@ -1,18 +1,51 @@
 package com.example.guidemaps.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.example.guidemaps.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.example.guidemaps.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.example.guidemaps.R;
 
+import java.util.ArrayList;
+
 public class UserDashboard extends AppCompatActivity {
+
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        featuredRecycler = findViewById(R.id.featured_recycler);
+
+        featuredRecycler();
     }
+
+    private void featuredRecycler() {
+
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonald_img,"McDonald's","Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.city_1,"Edenrobe","Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.city_2,"Sweet and Bakers","Lorem ipsum dolor sit amet, consectetur adipiscing elit."));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
+        GradientDrawable gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffeff400, 0xffaff600});
+
+    }
+
 }
