cebf07c92d1ad5749e4978ac2bec567b7d4875b4
davidblarig <75780229+davidblarig@users.noreply.github.com>
2022-06-05 23:30:09 +0200

    Form Validation

--- app/src/main/java/com/example/guidemaps/Common/LoginSignup/SignUp.java
+++ app/src/main/java/com/example/guidemaps/Common/LoginSignup/SignUp.java
@@ -13,12 +13,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.guidemaps.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Locale;
 
 public class SignUp extends AppCompatActivity {
 
     ImageView backBtn;
     Button next, login;
     TextView titleText;
+
+    TextInputLayout fullName, username, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +36,20 @@ public class SignUp extends AppCompatActivity {
         login = findViewById(R.id.signup_login_button);
         titleText = findViewById(R.id.signup_title_text);
 
+        fullName = findViewById(R.id.signup_fullname);
+        username = findViewById(R.id.signup_username);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+
         backBtn.setOnClickListener(view -> SignUp.super.onBackPressed());
 
     }
 
     public void callNextSignupScreen(View view) {
+
+        if(!validateFullName() | !validateUsername() | !validateEmail() | !validatePassword()){
+            return;
+        }
 
         Intent intent = new Intent(getApplicationContext(),SignUp.class);
 
@@ -69,6 +83,82 @@ public class SignUp extends AppCompatActivity {
             startActivity(intent,options.toBundle());
         } else startActivity(intent);
 
+    }
+
+    // Validation Functions
+    private boolean validateFullName() {
+        String val = fullName.getEditText().getText().toString().trim();
+
+        if(val.isEmpty()){
+            fullName.setError("El campo no puede estar vacío");
+            return false;
+        } else {
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "\\A\\w{1,20}\\z";
+
+        if(val.isEmpty()) {
+            username.setError("El campo no puede estar vacío");
+            return false;
+        } else if(val.length()>20) {
+            username.setError("¡El nombre de usuario es demasiado largo!");
+            return false;
+        } else if(!val.matches(checkspaces)) {
+            username.setError("¡No se permiten espacios en blanco!");
+            return false;
+        } else {
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString().trim();
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(val.isEmpty()) {
+            email.setError("El campo no puede estar vacío");
+            return false;
+        } else if(!val.matches(checkEmail)) {
+            email.setError("¡Email Inválido!");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString().trim();
+        String checkPassword =  "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+                //"(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=S+$)" +           //no white spaces
+                ".{4,}" +               //at least 4 characters
+                "$";
+
+        if(val.isEmpty()) {
+            password.setError("El campo no puede estar vacío");
+            return false;
+        } else if(!val.matches(checkPassword)) {
+            password.setError("¡La contraseña debe contener 4 caracteres!");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
     }
 
 }
